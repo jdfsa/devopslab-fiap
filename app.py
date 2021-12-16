398d04ae464448a8a954a837cb49d90b06ab452a
Jacques Sa <jacques.sa@gmail.com>
2021-12-16 00:42:46 -0300

    mensagem default

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World maneiro"
+    return "ambiente de teste rodando no heroku"
 
 if __name__ == '__main__':
     app.run()
